42c2f458bdb195e1487191c3c15fbddb37f82a43
Subash Tiwari <mrsubasht@gmail.com>
2025-03-02 15:02:28 -0600

    Dashboard Card Added

---
+++ frontend/barrier-free-communication-frontend/src/components/Card/Card.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+import './Card.css'
+
+const Card = ({theme, setTheme, text, image}) => {
+  return (
+    <div className="card">
+        {theme == 'light' &&
+        <div style={{color: 'white'}}>
+            <img src={image} alt="" className="card-image"></img>
+            <p style={{color: 'black'}} className='card-title'>{text}
+            </p>
+            <button className='card-button'>Audio to ASL</button>
+        </div>}
+
+        {theme == 'dark' &&
+        <div >
+            <img src={image} alt="" className="card-image"></img>
+            <p style={{color: 'white'}} className='card-title'>{text}
+            </p>
+            <button className='card-button'>Audio to ASL</button>
+        </div>}
+
+    </div>
+
+  )
+}
+
+export default Card
--- frontend/barrier-free-communication-frontend/src/components/Dashboard.jsx
+++ frontend/barrier-free-communication-frontend/src/components/Dashboard.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
+import Card from './Card/Card'
+import AudiotoASL from '../assets/AudiotoASL.png'
+import ASLtoText from '../assets/ASLtoText.png'
+import Captioning from '../assets/Captioning.png'
 
 const Dashboard = ({theme, setTheme}) => {
-  const toggle_mode = ()=>{
-    theme == 'light' ? setTheme('dark') : setTheme('light')
-}
 
   return (
     <div>
-      {theme == 'light' && <div style={{color: 'black'}}>Dashboard</div>}
-      {theme == 'dark' && <div style={{color: 'white'}}>Dashboard</div>}
-      </div>
+
+          <Card theme={theme} setTheme={setTheme} text={'Provide the audio input to view the ASL animation and to transcribe the audio into multiple languages.'} image={AudiotoASL}/>
+          <Card theme={theme} setTheme={setTheme} text={'Provide the American Sign Language video to view the text output and transcription to other languages.'} image={ASLtoText}/>
+          <Card theme={theme} setTheme={setTheme} text={'Provide the social media URL(Youtube) to view the suitable caption.'} image={Captioning}/>
+
+    </div>
     
   )
 }
